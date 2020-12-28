a59c0ea020e60c4c30ffbf1c5f6e0137a750df83
SikiTheBest <muaaz.sikiric@gmail.com>
2020-12-28 20:31:24 +0100

    added joi validation

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const Joi = require('joi');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -15,13 +16,16 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/add', (req, res) => {
+  const { error } = validateBurger(req.body);
+  if(error) return res.status(400).send(error.details[0].message);
+
   fs.readFile("./db.json", "utf8", (err, data) => {
     if (err) return console.log("File read failed:", err);
 
     jsonData = JSON.parse(data);
     const burger = {
       id: jsonData.burgers.length + 1,
-      name: req.body.name || "no name"
+      name: req.body.name
     };
     jsonData.burgers.push(burger);
 
@@ -32,6 +36,14 @@ app.post('/add', (req, res) => {
 
   res.redirect('/');
 });
+
+function validateBurger(burger) {
+  const schema = Joi.object({
+    name: Joi.string().min(5).required()
+  });
+
+  return schema.validate(burger);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
